864c7062f09b2e4e9f63eab4e119f9dff2cf00e9
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2021-08-10 20:20:33 +0530

    Initial commit

---
+++ website/users/views.py
@@ -0,0 +1,31 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+from .forms import SignUpForm
+
+
+def signup_view(request):
+	if request.user.is_authenticated:
+		return redirect('users:home')
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			return redirect('users:home')
+		else:
+			messages.error(request, 'Correct the errors below')
+	else:
+		form = SignUpForm()
+
+	return render(request, 'app/signup.html', {'form': form})
+
+
+def home_view(request):
+	return render(request, 'app/home.html')
